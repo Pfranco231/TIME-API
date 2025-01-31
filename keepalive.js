980b1510a86d51f92b8d19883bd97149243272a5
Pfranco231 <142932424+Pfranco231@users.noreply.github.com>
2025-01-31 16:07:36 +0000

    v2.0.6 Finished 21/1/2025

--- keepalive.js
+++ keepalive.js
@@ -1,14 +1,14 @@
-import { http } from "http";
+import https from "https";
 
 function keepAlive() {
-    http.get('https://humble-robot-5gq5v6v955492vxvv-8080.app.github.dev/', (res) => {
+    https.get('https://humble-robot-5gq5v6v955492vxvv-8080.app.github.dev/', (res) => {
         res.on('data', () => {});
         res.on('end', () => {
-            console.log(`Solicitud GET realizada correctamente a las: ${new Date().toLocaleString()}`)
+            // console.log(`Solicitud GET realizada correctamente a las: ${new Date().toLocaleString()}`)
         });
     }).on('error', (e) => {
         console.error(`Error: ${e.message}`);
     });
 }
 
-setInterval(keepAlive, 1 * 60 * 1000); // 5 minutos
+setInterval(keepAlive, 5 * 60 * 1000); // 5 minutos
